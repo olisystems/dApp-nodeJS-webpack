f505c6615e7d7a0d7679e26d07e1382d5db8a72b
m-yahya <yahya_gis@hotmail.com>
2020-07-24 14:03:22 +0500

    Config eth gas reporter

--- app/truffle-config.js
+++ app/truffle-config.js
@@ -82,6 +82,10 @@ module.exports = {
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      excludeContracts: ['Migrations'],
+    },
   },
 
   // Configure your compilers

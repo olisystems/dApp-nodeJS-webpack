540a1cd1e779261deeb0d0cd26bffc8e0640a62a
m-yahya <yahya_gis@hotmail.com>
2020-07-25 14:59:10 +0500

    Create Provider module

---
+++ app/src/Provider.js
@@ -0,0 +1,12 @@
+const Web3 = require('web3')
+
+class Provider {
+  constructor() {
+    //setup web3 provider
+    this.web3 = new Web3(
+      new Web3.providers.HttpProvider('http://localhost:9545'),
+    )
+  }
+}
+
+module.exports = Provider

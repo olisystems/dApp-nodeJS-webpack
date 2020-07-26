e35cfddf93721954b0d9b31e6f90f748521a1b01
m-yahya <yahya_gis@hotmail.com>
2020-07-26 15:19:16 +0500

    Deploy and write contract metadata to Metadata.js

--- app/migrations/2_deploy_contracts.js
+++ app/migrations/2_deploy_contracts.js
@@ -1,5 +1,40 @@
 const Registration = artifacts.require('Registration')
+const fs = require('fs')
+
+const path = '/../src/Metadata.js'
 
 module.exports = function (deployer) {
-  deployer.deploy(Registration)
+  // Use deployer to state migration tasks.
+  deployer.deploy(Registration).then(() => {
+    fs.writeFile(
+      __dirname + path,
+      'const ADDRESS = ' + "'" + Registration.address + "';",
+      (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+        }
+      },
+    )
+
+    fs.appendFile(
+      __dirname + path,
+      '\nconst ABI = ' + JSON.stringify(Registration.abi) + ';',
+      (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          fs.appendFile(
+            __dirname + path,
+            '\nmodule.exports = { ADDRESS, ABI };',
+            (err) => {
+              if (err) {
+                console.log(err)
+              }
+            },
+          )
+        }
+      },
+    )
+  })
 }

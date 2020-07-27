ae29061ed1554244c585b95849943474508091a1
m-yahya <yahya_gis@hotmail.com>
2020-07-27 11:06:14 +0500

    Config and integrate webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new CleanWebpackPlugin()],
+}
